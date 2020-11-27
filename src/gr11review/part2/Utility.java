efefaa3c33b5332df3529245ed81e28204f3193e
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2020-11-27 14:30:54 +0000

    create method 1 code

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -1,5 +1,32 @@
 package gr11review.part2;
 
 public class Utility {
-  testing  
+
+  public static void main(String[] args){
+
+  }
+
+  public static String zipZap(String str) {
+
+    String strWord = "";
+    int intCount;
+
+    if (str.length() <= 3) return str;
+
+    for (intCount = 0; intCount < str.length() - 2; intCount++) {
+      if (str.substring(intCount, intCount + 1).equals("z") && str.substring(intCount + 2, intCount + 3).equals("p")) {
+        strWord = strWord + str.substring(intCount, intCount +1) + str.substring(intCount + 2, intCount + 3);
+        intCount = intCount + 2;
+        }else {
+          strWord = strWord + strsubstring(intCount, intCount + 1);
+
+        }
+
+    }
+
+    return strWord;
+
+
+
+  }
 }
