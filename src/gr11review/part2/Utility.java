65cdb7520e9973fce22c04096ee292d79d0d0d81
Kyle Mendes <kyle.mendes21@ycdsbk12.ca>
2020-12-01 02:30:39 +0000

    fix method 1 again

--- src/gr11review/part2/Utility.java
+++ src/gr11review/part2/Utility.java
@@ -22,21 +22,6 @@ public class Utility {
 
     return strFinal;
 
-/**
-    for (intCount = 0; intCount < str.length() - 2; intCount++) {
-      if (str.substring(intCount, intCount + 1).equals("z") && str.substring(intCount + 2, intCount + 3).equals("p")) {
-        strWord = strWord + str.substring(intCount, intCount +1) + str.substring(intCount + 2, intCount + 3);
-        intCount = intCount + 2;
-        }else {
-          strWord = strWord + str.substring(intCount, intCount + 1);
-
-        }
-
-    }
-*/
-
-    // return strWord;
-
   }
 
 /**
